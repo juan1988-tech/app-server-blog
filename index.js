f6834d406660f4c004df7376f25db9426e87bd28
Juan David Franco <jfrancoh5@gmail.com>
2024-05-27 16:25:41 -0500

    creando el servidor

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-const { connection } = require('./database/connection')
+/*conexion a la base de datos*/
+const { connection } = require('./database/connection');
+const cors = require('cors');
 
 connection()
+
+/*creando el servidor*/
+
+const express = require('express');
+
+const app = express();
+
+app.use(cors())
+
+const port = 3000;
+
+app.listen(port,()=>{
+    console.log(`El servidor está corriendo en el puero ${port}`)
+})
+
+/*creando una ruta de prueba*/ 
+
+app.get("/probando",(req,res)=>{
+    console.log(`Dirigete a la ruta http://localhost:3000/probando`)
+    return res.status(200).send({
+        nombre: 'Juan David Franco',
+        edad: '35 annos'
+    })
+
+})
